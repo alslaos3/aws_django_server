86c662b05e7750ed69c207157bdb2d413b1f3f14
hjs <alslaos3@gmail.com>
2022-07-01 15:39:16 +0900

    add ALLOWED_HOSTS

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '$pypcyle_u)x(nj_k##(&#@_d@!i7uk@3nun=qhpuow&ljhdns'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
